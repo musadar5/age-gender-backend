fd6fbbba83bc870c66c840887d7c3c25d9789449
Musa Dar <darmusa572@gmail.com>
2025-05-19 17:30:14 +0500

    some changes

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
+import os
+# Disable all GPU usage (important on Railway)
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import numpy as np
 from PIL import Image
-import io
 from tensorflow.keras.models import load_model
-import tensorflow as tf
 
 app = Flask(__name__)
 CORS(app)
 
-# Load model
+# Load the model
 model = load_model("model/age_gender_model.keras")
 
-# Prediction function
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'image' not in request.files:
         return jsonify({'error': 'No image uploaded'}), 400
 
     image_file = request.files['image']
-    image = Image.open(image_file).convert('L')
+    image = Image.open(image_file).convert('L')  # Convert to grayscale
     image = image.resize((128, 128))
     img_array = np.array(image).reshape(1, 128, 128, 1) / 255.0
 
@@ -34,5 +35,6 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    # Use Railway-friendly port config
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
